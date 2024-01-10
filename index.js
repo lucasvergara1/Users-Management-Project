d14dbafd754e3ed6d30b8d4efbf98d37f3120f30
Lucas <lucas.vergara@live.com>
2024-01-10 19:24:30 -0300

    :construction: Working in progress

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");//Selecionamos apenas o que tem nome - gender e que esta checado
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFilr");
-var admin = document.querySelector("#exampleInputAdmin");
-
-
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function(field, index){
 
-    if (field.name == "gender") {
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+    
+    event.preventDefault();
 
-        if (field.checked) {
-            user[field.name] = field;
+    fields.forEach(function(field, index){
+
+        if (field.name == "gender") {
+    
+            if (field.checked === true) {
+                user[field.name] = field;
+            }
+    
+        } else {
+    
+            user[field.name] = field.value;
         }
+    
+    });
+    
+    console.log(user);
 
-    } else {
-
-        user[field.name] = field.value;
-    }
-
-});
-
-console.log(user);
-
-document.querySelectorAll("button").forEach(function(){
-
-    this.addEventListener("click",function(){
-        console.log("clicou!");
-    })
 
 });
-teste de branch KeyboardEvent
-console.log() 
