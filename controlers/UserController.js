9c411cb59b8d80d24d22f9f1b65b60e85a898207
Lucas <lucas.vergara@live.com>
2024-01-10 23:09:47 -0300

    :construction: Fix innerHTML and the option yes or nome related if the user is admin or not

--- controlers/UserController.js
+++ controlers/UserController.js
@@ -62,7 +62,10 @@ class UserController {
 
             };
 
-            fileReader.readAsDataURL(file);
+            if (file) {fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
 
         });
 
@@ -80,9 +83,13 @@ class UserController {
                     user[field.name] = field.value
                 }
     
+            } else if(field.name == "admin") {
+                
+                user[field.name] = field.checked;
+
             } else {
     
-                user[field.name] = field.value
+                user[field.name] = field.value;
     
             }
     
@@ -104,19 +111,22 @@ class UserController {
     
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
-        `;
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
+            <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+    `;
+
+
+        this.tableEl.appendChild(tr);
 
     }
 
