b4e05c4f8d58e101f3d62513d219734efc3c023c
Lucas <lucas.vergara@live.com>
2024-01-12 00:36:09 -0300

    :sparkles: Create safety precautions in the form focusing in name, email and password

--- controlers/UserController.js
+++ controlers/UserController.js
@@ -82,8 +82,17 @@ class UserController {
     getValues(){
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index){
+
+            if(['name','email','password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
+
 
             if (field.name === "gender") {
     
@@ -102,6 +111,11 @@ class UserController {
             }
     
         });
+
+        if (!isValid) {
+            return false;
+
+        }
     
         return new User(
             user.name, 
